f911b89ce1644443fbff9c1e75b432390e4c2dc3
itose <s.itou.sys23@morijyobi.ac.jp>
2023-11-14 11:34:29 +0900

    2人文BIM計算できるように変更

--- Main.java
+++ Main.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("身長を入力してください");
+        System.out.print("１人目の身長を入力してください");
         double height = scanner.nextDouble();
-        System.out.print("体重を入力してください");
+        System.out.print("２人目身長を入力してください");
+        double height2 = scanner.nextDouble();
+        System.out.print("１人目体重を入力してください");
         double weight = scanner.nextDouble();
+        System.out.print("２人目体重を入力してください");
+        double weight2 = scanner.nextDouble();
         scanner.close();
-        System.out.printf("あなたのBMI値は%.2fです%n", weight / Math.pow(height / 100, 2));
+        System.out.printf("１人目のBMI値は%.2fです%n", weight / Math.pow(height / 100, 2));
+        System.out.printf("2人目のBMI値は%.2fです%n", weight2 / Math.pow(height2 / 100, 2));
     }
 }
